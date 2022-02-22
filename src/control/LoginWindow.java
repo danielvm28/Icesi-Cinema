d1e2f83efb86323142da1937c60564b0c750fbd2
Daniel Valencia <daniel.valencia.manrique@gmail.com>
2022-02-21 21:41:45 -0500

    Class Diagram and Login Window

---
+++ src/control/LoginWindow.java
@@ -0,0 +1,19 @@
+package control;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginWindow {
+	 	@FXML
+	    private Button loginBTN;
+
+	    @FXML
+	    private TextField idTF;
+
+	    @FXML
+	    void login(ActionEvent event) {
+
+	    }
+}
