94d77b60ccb2990ff3f3345991865de2876d4b49
Camilo <jc_bernal2011@hotmail.com>
2022-02-25 14:59:06 -0500

    prueba

--- src/main/Main.java
+++ src/main/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application{
 		Scene scene = new Scene(parent);
 		Stage stage = new Stage();
 		stage.setScene(scene);
-		stage.show();
+		stage.show();//prueba
 	}
 
 }
