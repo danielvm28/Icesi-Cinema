917b4bcd1575e99fea9c62a28679a0d31b14ef56
Daniel Valencia <daniel.valencia.manrique@gmail.com>
2022-02-25 18:06:47 -0500

    Se añade main menu, foto Icesi

---
+++ src/control/MainMenuController.java
@@ -0,0 +1,31 @@
+package control;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainMenuController {
+	@FXML
+    private Button addMovieBTN;
+
+    @FXML
+    private Button registerUserBTN;
+
+    @FXML
+    private Button reportBTN;
+
+    @FXML
+    void addMovie(ActionEvent event) {
+
+    }
+
+    @FXML
+    void generateReport(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerUser(ActionEvent event) {
+
+    }
+}
